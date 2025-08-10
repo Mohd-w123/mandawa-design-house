bcf6f0e40bcdc830cc63098858045f1e4924e734
danishRajawat <ibnfarooq070@gmail.com>
2025-08-10 16:21:36 +0530

    manufecturing section images problem solved

--- src/components/Global/HomePage/ManufacturingSection/ManufacturingSection.js
+++ src/components/Global/HomePage/ManufacturingSection/ManufacturingSection.js
@@ -16,20 +16,19 @@ const ManufacturingSection = () => {
       ].map((item, idx) => (
         <div key={idx} className="relative group flex flex-col gap-[30px] cursor-pointer">
           {/* Image */}
-          <div className="relative aspect-[4/3] h-[440px]">
-            <Image
-              src={item.src}
-              fill
-              alt={item.title}
-              className="object-cover"
-            />
-            {/* Hover overlay */}
-            <div className="absolute top-0 w-full h-full flex justify-center items-center bg-black/50 opacity-0 group-hover:opacity-100 transition-all duration-300">
-              <h4 className="mt-5 group-hover:mt-0 transition-all duration-300 font-belleza text-lg sm:text-xl md:text-2xl lg:text-[34px] text-white text-center px-4">
-                {item.title}
-              </h4>
-            </div>
-          </div>
+          <div className="relative aspect-[4/3] w-full">
+  <Image
+    src={item.src}
+    fill
+    alt={item.title}
+    className="object-cover"
+  />
+  <div className="absolute top-0 w-full h-full flex justify-center items-center bg-black/50 opacity-0 group-hover:opacity-100 transition-all duration-300">
+    <h4 className="mt-5 group-hover:mt-0 transition-all duration-300 font-belleza text-lg sm:text-xl md:text-2xl lg:text-[34px] text-white text-center px-4">
+      {item.title}
+    </h4>
+  </div>
+</div>
 
           {/* Static label */}
           <div className="flex justify-center relative bottom-0 opacity-100 group-hover:bottom-5 group-hover:opacity-0 transition-all duration-300">
